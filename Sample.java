954a6ba8c0e8e93f7a5c12a00f3f0b418fc2b4c4
Naresh Babu Jaya Chand <50233615+NareshBabuGit@users.noreply.github.com>
2024-02-17 20:19:01 +0530

    sample

--- Sample.java
+++ Sample.java
@@ -1,24 +1,7 @@
-import java.util.*;
- 
- class Sample{ 
- 
- 
+class Sample{
+
   public static void main(String args[]){
- 
- 
-     Scanner sc=new Scanner(System.in);
- 
-      System.out.print("Enter a value "); 
- 
-      int a=sc.nextInt();
-      System.out.print("Enter Name : "); 
-   
-      String name=sc.next();
-   
-       System.out.print("Enter your height : "); 
-   
-      float h=sc.nextFloat();
-  System.out.println("Entered  value "+a+" Name "+name+"  Height "+h); 
-   
-   }
+
+    System.out.println("Welcome to java"); 
+  }
 }
